787b15041aefceaa081c9bba7ce4d06996a25c10
Carlos Daniel <danielflop98@gmail.com>
2022-12-11 22:39:47 -0500

    Update settings.py

--- Portafolio/settings.py
+++ Portafolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dlr3vn-0xxw*0d@d$deg$p7bebd&2d)08&bli911=ck5x%23p0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
